33bd2bf5d563d8f4129bb08cb509bc7081a90a03
SeneTeranga <senedevs@gmail.com>
2025-01-23 11:48:16 +0000

    first project cv

---
+++ TPCV/TPCV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TPCV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
